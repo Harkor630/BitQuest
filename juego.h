57d61a5039ba069ef609675bfbb8e84c03eb2f43
monsectzz <monserratcortezz05@gmail.com>
2026-06-14 17:09:14 -0600

    Agregar juego.h con definiciones, constantes y prototipos

--- juego.h
+++ juego.h
@@ -1,11 +1,10 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 
-#define FILAS 60 
-#define COLS 60 
+#define FILAS 61
+#define COLS 60
 #define VISTA 20
 
-//Caracteres para representar el mapa
 #define PARED   '#'
 #define CAMINO  '.'
 #define JUGADOR 'P'
@@ -14,7 +13,6 @@
 #define PUERTA  'D'
 #define SALIDA  'E'
 
-//Estado del Jugador
 typedef struct {
     int fila;
     int col;
@@ -25,9 +23,10 @@ typedef struct {
 
 //Funciones en C
 void imprimir_mapa(char mapa[FILAS][COLS], Jugador *j, int nivel);
-void mover_jugador(char mapa[FILAS][COLS], Jugador *j);
+int  mover_jugador(char mapa[FILAS][COLS], Jugador *j);
 void resumen_nivel(Jugador *j, int total_monedas, int nivel);
-void resumen_final(int monedas_total, int pasos_total, int puntaje);
+void resumen_final(int monedas_total, int monedas_posibles,
+                int pasos_total, int puntaje);
 
 //Funciones en NASM
 extern long long contar_caracter(char *mapa, int total, char c);
@@ -36,4 +35,4 @@ extern long long calcular_puntaje(int monedas, int pasos, int niveles);
 extern long long detectar_objeto(char *mapa, int cols, int fila, int col, char obj);
 extern long long contar_libres(char *mapa, int total);
 
-#endif 
+#endif
